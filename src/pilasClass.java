a0a6a2c51acf6fdd6e1a97e54e28bacd39a65b03
Nefta11 <neftaliarturohernandez@gmail.com>
2023-11-23 17:11:05 -0600

    Update...

--- src/pilasClass.java
+++ src/pilasClass.java
@@ -1,5 +1,27 @@
 public class pilasClass {
-    
+    int vectorPila [];
+    int cima;
+
+    //Constructor
+    public pilasClass(int tamaño){
+        vectorPila = new int[tamaño];
+        cima=-1;
+    }
+
+    //Método para insertar un valor (Push)
+    public void empujar(int dato){
+        cima++;
+        vectorPila[cima]=dato;
+    }
+
+    //Método para eliminar un valor (pop)
+    public int eliminar(){
+        int eliminado = vectorPila[cima];
+
+        cima--;
+
+        return eliminado;
+    }
 
 
 }
